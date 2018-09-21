8f12b96576ff743f67fbab9502974689c8b21986
Matt <mjkoza5@gmail.com>
2018-09-21 11:53:09 -0500

    added some comments

--- healthproject.py
+++ healthproject.py
@@ -1,12 +1,12 @@
-import random #this is Pythons Random Module that we imported. Generates Random numbers. always put at top
+import random #imported Pythons Random Module function. Generates Random numbers. always put at top
 
 #variables
 health = 50
 difficulty = 2  #difficulty is 1-3. 1 being the easiest. 
 
 
-potion_health = int(random.randint(25,50) / difficulty)  #Here we are casting. Division changes the type to a float.
-                                                         #so we add INT to keep it an integer or whole number. That is Casting
+potion_health = int(random.randint(25,50) / difficulty)  #Here had to use casting. Division changes the type to a float and gives us Decimal.
+                                                         #so we add INT to keep it an integer or whole number. That is Casting.
                                                         #random.randint is the random funtion
 health = health + potion_health
 print(health)
