76cce31c84b95bac2cb08637657b1b8478a3e4b2
MattK <37481457+mjkoza5@users.noreply.github.com>
2018-10-10 12:07:49 -0500

    Update baby_convo_whileLoop_project.py

--- baby_convo_whileLoop_project.py
+++ baby_convo_whileLoop_project.py
@@ -1,6 +1,6 @@
 import random
 
-questions = ["Why is the earth round? ", "Why do dogs bark? ", "Why do you suck at python? "]
+questions = ["Why is the earth round? ", "Why do dogs bark? ", "Why is python awesome? "]
 
 question = random.choice(questions)             #using the random.choice function. import random module
 answer = input(question).lower().strip()
